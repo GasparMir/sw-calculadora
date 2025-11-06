cd9f44928783398bdd6621113baad1531b7b8617
J. Gaspar Miranda Lamadrid <89092725+GasparMir@users.noreply.github.com>
2025-11-05 22:53:05 -0600

    fix: fixed files

--- sw.js
+++ sw.js
@@ -1,48 +1,36 @@
 const CACHE_NAME = 'app-shell-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
+const urlsToCache = [
+    './',
+    './index.html',
+    './app.js',
+    './styles.css',
+    './manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-  console.log('[SW] Instalando…');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[SW] Activado');
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => k !== CACHE_NAME && caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-  let pathname = url.pathname;
-  if (pathname === '/') pathname = './';
-  else pathname = '.' + pathname;
-
-  const isAppShell = ASSETS.includes(pathname);
-
-  if (isAppShell) {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
+// Instalar Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Cache abierto');
+            return cache.addAll(urlsToCache);
+        })
     );
-  } else {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    );
-  }
 });
 
+// Interceptar peticiones y servir desde caché si es posible
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch(() => caches.match('./index.html'));
+        })
+    );
+});
+
+// Limpiar cachés viejos al activar nuevo SW
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => key !== CACHE_NAME && caches.delete(key))
+        ))
+    );
+});
